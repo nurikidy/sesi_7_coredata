4a1f7b5460992e13f8a530f688734c7e6cb6791d
Nuri Abidin <nuri.abidin@gmail.com>
2023-01-06 22:17:03 +0700

    sesi 7 core data

---
+++ MemberDicoding/Base/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  MemberDicoding
+//
+//  Created by Gilang Ramadhan on 24/06/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
